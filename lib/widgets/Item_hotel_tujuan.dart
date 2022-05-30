6b3a7200e0ad2b1ee5b8170762c65ce9217b8b5e
khoir-roni <khoir.roni@gmail.com>
2022-05-30 10:01:48 +0700

    add ui refferenc

--- lib/widgets/Item_hotel_tujuan.dart
+++ lib/widgets/Item_hotel_tujuan.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_travel_ui_starter/models/hotel_model.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class HotelCarousel extends StatelessWidget {
   @override
